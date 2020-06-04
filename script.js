aaa6a6e0e37e81820fbcfc9908cb2a029dee4e82
Charles Ivia <iviacharles@gmail.com>
2020-06-04 15:47:01 +0300

    Fetched dom elements

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+
+const totalTime = 7500;
+const breatheTime = (totalTime / 2) * 2;
+const holdTime = totalTime / 5;
+
+
