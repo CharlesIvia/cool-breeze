dfd31eb41789a1b0d403c8aceb61697fa87a5bc5
Charles Ivia <iviacharles@gmail.com>
2020-06-04 15:55:22 +0300

    Added breathe animation logic

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ const totalTime = 7500;
 const breatheTime = (totalTime / 2) * 2;
 const holdTime = totalTime / 5;
 
+breatheAnimation();
 
+let breatheAnimation = () => {
+  text.innerText = "Breathe In";
+  container.className = "containter-grow";
+
+  setTimeout(() => {
+    text.innerText = "Hold Breathe";
+
+    setTimeout(() => {
+      text.innerText = "Breathe Out!";
+      container.className = "container-shrink";
+    }, holdTime);
+  }, breatheTime);
+};
+
+setInterval(breatheAnimation, totalTime);
